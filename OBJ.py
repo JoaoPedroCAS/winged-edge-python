90c4eeb85f2b86f0918ac34ca9c56bf3ae287957
Joao Pedro Cardoso Amancio de Sa <70914320+JoaoPedroCAS@users.noreply.github.com>
2023-09-18 22:42:29 -0300

    Alterações das funções para os requisitos da atividade

--- OBJ.py
+++ OBJ.py
@@ -13,7 +13,10 @@ class Edge:
     def __init__(self, vertex1, vertex2):
         self.vertex1 = vertex1  # Inicializa o primeiro vértice da aresta
         self.vertex2 = vertex2  # Inicializa o segundo vértice da aresta
-        self.faces = []  # Inicializa uma lista vazia para armazenar as faces conectadas a esta aresta
+        self.face_ids = []  # Inicializa uma lista vazia para armazenar os IDs das faces conectadas a esta aresta
+
+    def add_face(self, face_id):
+        self.face_ids.append(face_id)
 
 # Definindo a classe Face (face)
 class Face:
@@ -43,7 +46,7 @@ def build_winged_edge_structure(file_path):
                 faces.append(face)  # Adiciona a face à lista de faces
 
     # Conecta vértices, arestas e faces
-    for face in faces:
+    for face_id, face in enumerate(faces, start=1):
         for i, vertex in enumerate(face.vertices):
             vertex.faces.append(face)  # Adiciona a face à lista de faces do vértice
 
@@ -61,57 +64,45 @@ def build_winged_edge_structure(file_path):
                 edge = Edge(v1, v2)  # Cria uma nova aresta
                 edges.append(edge)  # Adiciona a aresta à lista de arestas
 
-            edge.faces.append(face)  # Adiciona a face à lista de faces da aresta
+            edge.add_face(face_id)  # Adiciona o ID da face à lista de IDs de faces da aresta
             face.edges.append(edge)  # Adiciona a aresta à lista de arestas da face
             vertex.edges.append(edge)  # Adiciona a aresta à lista de arestas do vértice
 
     return vertices, edges, faces  # Retorna as listas de vértices, arestas e faces
 
 # Função para listar informações sobre vértices
-def list_vertex_info(vertices,selected_vertex_id):
-    Vlist = ""
-    # Verificação de ID válido para vértices
-    if selected_vertex_id < 1 or selected_vertex_id > len(vertices):
+def list_vertex_info(vertex, edges):
+    if not vertex:
         sg.popup("Error: Vertex does not exist!")
-    else:
-        vertex = vertices[selected_vertex_id - 1]  # Obtém o vértice correspondente com base no ID
-        shared_faces = vertex.faces  # Lista de faces compartilhadas pelo vértice
+        return
 
-        if not shared_faces:
-            sg.popup(f"No faces shared by vertex {selected_vertex_id}")
-        else:
-            
-            for face in shared_faces:
-                vertex_ids_face = ', '.join(str(v.vertex_id) for v in face.vertices)
-                Vlist = Vlist + '\n' +  str(vertex_ids_face)
-                #print(f"Face vertices: [{vertex_ids_face}]")
-            sg.popup(f"Faces shared by vertex {selected_vertex_id}:", Vlist)
-    
-    
+    shared_edges = vertex.edges  # Lista de arestas compartilhadas pelo vértice
+
+    if not shared_edges:
+        sg.popup(f"No edges shared by vertex {vertex.vertex_id}")
+    else:
+        edge_ids = [edges.index(edge) + 1 for edge in shared_edges]
+        sg.popup(f"Edges shared by vertex {vertex.vertex_id}:", ', '.join(map(str, edge_ids)))
+
 # Função para listar informações sobre arestas
-def list_edge_info(edges,selected_edge_id):
-    Elist = ""
-    
+def list_edge_info(edges, selected_edge_id):
+    Flist = ""
+
     # Verificação de ID válido para arestas
     if selected_edge_id < 1 or selected_edge_id > len(edges):
         sg.popup("Error: Edge does not exist!")
     else:
         edge = edges[selected_edge_id - 1]  # Obtém a aresta correspondente com base no ID
-        shared_faces = edge.faces  # Lista de faces compartilhadas pela aresta
+        shared_faces = edge.face_ids  # Lista de IDs de faces compartilhadas pela aresta
 
         if not shared_faces:
             sg.popup(f"No faces shared by edge {selected_edge_id}")
         else:
-            for face in shared_faces:
-                vertex1_id = face.vertices[0].vertex_id
-                vertex2_id = face.vertices[1].vertex_id
-                Elist = Elist +  (f"Edge vertices: {vertex1_id}, {vertex2_id}" + "\n")  
-            sg.popup(f"Faces shared by edge {selected_edge_id}:",Elist)
+            sg.popup(f"Faces shared by edge {selected_edge_id}:", ', '.join(map(str, shared_faces)))
 
 # Função para listar informações sobre faces
-def list_face_info(faces,selected_face_id):
+def list_face_info(faces, selected_face_id):
     Vlist = ""
-    # Verificação de ID válido para faces
     # Verificação de ID válido para faces
     if selected_face_id < 1 or selected_face_id > len(faces):
         sg.popup("Error: Face does not exist!")
@@ -125,34 +116,33 @@ def list_face_info(faces,selected_face_id):
             for shared_vertex in shared_vertices:
                 Vlist = Vlist + (f"Vertex ID: {shared_vertex.vertex_id}" + "\n")
             sg.popup(f"Vertices in face {selected_face_id}:", Vlist)
-            
 
+sg.theme('DarkAmber')  # Adicione um toque de cor
 
+# Todas as coisas dentro da sua janela.
+layout = [[sg.Text('Winged Edge Structure')],
+          [sg.Text('Enter the name of the object you want to load (cube, square, circle, etc.):'),
+           sg.InputText()],
+          [sg.Button('Ok'), sg.Button('Cancel')]]
 
-
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Winged Edge Structure')],
-            [sg.Text('Enter the name of the object you want to load (cube, square, circle, etc.):'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
-
-# Create the Window
+# Crie a Janela
 window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+
+# Loop de Eventos para processar "eventos" e obter os "valores" das entradas
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # Se o usuário fechar a janela ou clicar em Cancel
         break
 
     chosen_object = values[0]
-    path = os.path.dirname(os.path.abspath(__file__))   #Obtem a pasta atual
+    path = os.path.dirname(os.path.abspath(__file__))  # Obtém a pasta atual
     path = path + "\\" + chosen_object + ".obj"
 
     object_file = path
-    
+
     try:
         vertices, edges, faces = build_winged_edge_structure(object_file)
-        sg.popup('Object loaded succefully!')
+        sg.popup('Object loaded successfully!')
         break
     except FileNotFoundError:
         print("Object not found. Please try again.")
@@ -160,38 +150,32 @@ while True:
 
 window.close()
 
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter the vertex ID you want to access faces:'), sg.InputText()],
-            [sg.Text('Enter the edge ID you want to access edges:'), sg.InputText()],
-            [sg.Text('Enter the face ID you want to access vertices:'), sg.InputText()],
-            [sg.OK(), sg.Cancel()]]
+layout = [[sg.Text('Some text on Row 1')],
+          [sg.Text('Enter the vertex ID you want to access edges:'), sg.InputText()],
+          [sg.Text('Enter the edge ID you want to access faces:'), sg.InputText()],
+          [sg.Text('Enter the face ID you want to access vertices:'), sg.InputText()],
+          [sg.OK(), sg.Cancel()]]
 
-# Create the Window
+# Crie a Janela
 window = sg.Window('Window Title', layout)
-# Event Loop to process "events"
-while True:             
+
+# Loop de Eventos para processar "eventos"
+while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Cancel'):
         break
-    
-    if values[0] == "" and values[1] == "" and  values[2] == "" :             #no informatio was given 
-        sg.popup('Error: No information requested!')
-        continue    
-    if (values[0] != "" and values[1] != "") or  (values[0] != "" and values[2] != "") or  (values[1] != "" and values[2] != ""):
-        sg.popup('Error: More then one information requested!')  
-        continue
 
-    if  values[0] != "":                          #fist fild requested
+    if values[0] != "":  # Primeiro campo solicitado
         operation = int(values[0])
-        list_vertex_info(vertices,operation) 
-    
-    if  values[1] != "":                          #second fild requested
+        selected_vertex = vertices[operation - 1] if 1 <= operation <= len(vertices) else None
+        list_vertex_info(selected_vertex, edges)
+
+    if values[1] != "":  # Segundo campo solicitado
         operation = int(values[1])
-        list_edge_info(edges,operation)
+        list_edge_info(edges, operation)
 
-    if  values[2] != "":                          #third fild requested
+    if values[2] != "":  # Terceiro campo solicitado
         operation = int(values[2])
-        list_face_info(faces,operation)
-
+        list_face_info(faces, operation)
 
 window.close()
